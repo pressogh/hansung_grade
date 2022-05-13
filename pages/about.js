9ef100354a17a797ca307d83add89338d773df36
PRESSO <user1205@naver.com>
2022-05-13 15:06:44 +0900

    Add Temp about page

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Title from "../components/Title";
+
+export default function Home() {
+  return (
+      <div>
+        	<Title title="한움 | about" />
+            <div>
+                
+            </div>
+
+            <style jsx>{`
+                .loading {
+                    display: flex;
+                    self-direction: center;
+                    justify-self: center;
+                }
+            `}</style>
+      </div>
+  );
+}
