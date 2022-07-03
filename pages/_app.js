8c40366c08420bfff69a2658a9e45a273dc1109f
PRESSO <caff1nepill@gmail.com>
2022-07-03 22:57:00 +0900

    Separate LeftMenu

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,36 @@
-import {NextUIProvider} from "@nextui-org/react";
-import { RecoilRoot } from "recoil";
-import Layout from "../layouts/Layout";
-import {ToastContainer, Zoom, Slide} from "react-toastify";
+import { NextUIProvider } from '@nextui-org/react'
+import { RecoilRoot } from 'recoil'
+import { ToastContainer, Zoom, Slide } from 'react-toastify'
+import Layout from '../layouts/Layout'
 
-import 'react-toastify/dist/ReactToastify.min.css';
-
+import 'react-toastify/dist/ReactToastify.min.css'
 
 export default function App({ Component, pageProps }) {
     return (
-		<RecoilRoot>
-			<NextUIProvider>
-				<Layout>
-					<div>
-						<Component {...pageProps} />
-						<ToastContainer
-							position="top-center"
-							autoClose={3000}
-							pauseOnHover={false}
-							pauseOnFocusLoss={false}
-							hideProgressBar={true}
-							transition={Slide}
-							style={{ width: "40vw" }}
-							theme={"colored"}
-						/>
-					</div>
-				</Layout>
+        <RecoilRoot>
+            <NextUIProvider>
+                <Layout>
+                    <div>
+                        <Component {...pageProps} />
+                        <ToastContainer
+                            position="top-center"
+                            autoClose={3000}
+                            pauseOnHover={false}
+                            pauseOnFocusLoss={false}
+                            hideProgressBar={true}
+                            transition={Slide}
+                            style={{ width: '40vw' }}
+                            theme={'colored'}
+                        />
+                    </div>
+                </Layout>
 
-				<style jsx>{`
-					.container {
-						margin: 0 0vw 0 10vw;
-					}
-				`}</style>
-			</NextUIProvider>
-		</RecoilRoot>
-    );
+                <style jsx>{`
+                    .container {
+                        margin: 0 0 0 10vw;
+                    }
+                `}</style>
+            </NextUIProvider>
+        </RecoilRoot>
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import { gradeData, nowGradeData } from "../utils/States";
 import {getGrade, getInfo, getNowGrade} from "../utils/Api";
 import LineGraph from "../components/Graph/LineGraph";
 import RandomGraph from "../components/Graph/RandomGraph";
+import LeftMemu from "../components/LeftMemu";
 
 export default function Home() {
     const [grade, setGrade] = useRecoilState(gradeData);
@@ -58,18 +59,7 @@ export default function Home() {
             <Title title="한움" />
 
             <div>
-                <div className="lnb-border">
-                    <div className="lnb-item">
-                        <div>Line</div>
-                        <Switch
-                            checked={contentType}
-                            onChange={() => {
-                                setContentType(!contentType);
-                            }}
-                        />
-                        <div>bar</div>
-                    </div>
-                </div>
+                <LeftMemu contentType={contentType} setContentType={setContentType} />
             </div>
 
             <div>
@@ -108,23 +98,6 @@ export default function Home() {
                     padding: 1vw;
                     border-radius: 3vmin;
                     box-shadow: 0 10px 50px -3px rgba(0,0,0,0.1);
-                }
-                .lnb-border {
-                    display: flex;
-                    justify-content: center;
-                    width: 15vw;
-                    height: 40vh;
-                    background: white;
-                    padding: 1vw;
-                    border-radius: 3vmin;
-                    box-shadow: 0 10px 50px -3px rgba(0,0,0,0.1);
-                    margin: 15vh 5vw 0 0;
-                }
-                .lnb-item {
-                    display: flex;
-                    justify-content: space-between;
-                    width: 60%;
-                    border-bottom: 1px solid rgba(219, 219, 219, 1);
                 }
             `}</style>
         </div>
